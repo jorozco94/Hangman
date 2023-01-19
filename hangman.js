ad3d9b9fd0d655ab33b32872dd74d83bf154d5c8
jorozco94 <91165810+jorozco94@users.noreply.github.com>
2023-01-18 23:57:52 -0800

    chore: continued work on hangman

--- hangman.js
+++ hangman.js
@@ -1,9 +1,20 @@
 const Hangman = function(word, guesses) {
-  this.word = word;
+  this.word = word.toLowerCase().split("");
   this.guesses = guesses;
+  this.guessedLetters = ["b", "l"];
+}
+
+Hangman.prototype.getPuzzle = function() {
+  return this.word.map((letter) => {
+    if (letter === " ") return " "
+    else if (!this.guessedLetters.includes(letter)) return "*" 
+    else return letter
+  }).join("");
+  console.log(this.word)
+  return this.word.join("")
 }
 
 const game1 = new Hangman("bean", 6);
-const game2 = new Hangman("peanut", 10);
+const game2 = new Hangman("peanut b", 10);
 
-console.log(game2);
+console.log(game1.getPuzzle());
