2a18e0d40b624e18af08767bb056c2a72fbbca31
jorozco94 <91165810+jorozco94@users.noreply.github.com>
2023-01-22 13:21:14 -0800

    feat: basic guessing functionality added

--- hangman.js
+++ hangman.js
@@ -1,7 +1,12 @@
+// Create a method for making a guess
+// Accept a char for guessing
+// Add unique guesses to list of guesses
+// Decrement guesses left if a unique guess isn't a match
+
 const Hangman = function(word, guesses) {
   this.word = word.toLowerCase().split("");
   this.guesses = guesses;
-  this.guessedLetters = ["b", "l"];
+  this.guessedLetters = [];
 }
 
 Hangman.prototype.getPuzzle = function() {
@@ -12,6 +17,13 @@ Hangman.prototype.getPuzzle = function() {
   }).join("");
   console.log(this.word)
   return this.word.join("")
+}
+
+Hangman.prototype.makeGuess = function(letter) {
+  const guess = letter.toLowerCase();
+  if (!this.guessedLetters.contains(guess)) this.guessedLetters.push(guess);
+  // below - have to check that the guess we're checking is unique AND is a bad guess
+  if (!this.word.contains(guess)) this.guesses--;
 }
 
 const game1 = new Hangman("bean", 6);
